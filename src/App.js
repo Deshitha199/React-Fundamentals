f71b3b8ed5b42ed09b548bd679c79c6c24e4a043
Deshitha199 <IT20657628@my.sliit.lk>
2023-07-18 10:46:17 +0530

    Back initial setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import UseState from './components/useState/UseState';
-import UseStateArray from './components/useState/UseStateArray';
-import UseStateFunctionalCounter from './components/useState/UseStateFunctionalCounter';
-import UseStateObject from './components/useState/UseStateObject';
-
-
-
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='main-title'>React Hooks</h1>
-      <UseState />
-      <UseStateArray />
-      <UseStateObject />
-      <UseStateFunctionalCounter />
     </div>
   );
 }
