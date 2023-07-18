e99964057c397f576cdddf7f07dc11d7cd33528c
Deshitha199 <IT20657628@my.sliit.lk>
2023-07-18 14:01:30 +0530

    api-request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+
+  const [joke, setJoke] = useState('')
+  const [punchline, setPunchline] = useState('')
+
+  const getJoke = () => {
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+      .then((res) => {
+        setJoke(res.data.setup);
+        setPunchline(res.data.punchline);
+      })
+  }
+
   return (
     <div className="App">
+      <div>
+        <button onClick={getJoke}>
+          Get a Joke
+        </button>
+        <p>{joke}</p>
+        <p>{punchline}</p>
+
+      </div>
     </div>
   );
 }
