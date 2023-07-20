1b6146b3d88869f45e12e9481be1ee28247bb530
Deshitha199 <IT20657628@my.sliit.lk>
2023-07-20 13:58:30 +0530

    joke commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ function App() {
 
   const [joke, setJoke] = useState('')
   const [punchline, setPunchline] = useState('')
+  const [id, setId] = useState('')
 
   const getJoke = () => {
     axios.get('https://official-joke-api.appspot.com/random_joke')
       .then((res) => {
+        setId(res.data.id)
         setJoke(res.data.setup);
         setPunchline(res.data.punchline);
       })
   }
+
+  // const getJokeById = () => {
+  //   axios.
+  // }
 
   return (
     <div className="App">
@@ -20,9 +26,12 @@ function App() {
         <button onClick={getJoke}>
           Get a Joke
         </button>
+        <p>{id}</p>
         <p>{joke}</p>
         <p>{punchline}</p>
-
+      </div>
+      <div>
+        get Joke by id
       </div>
     </div>
   );
